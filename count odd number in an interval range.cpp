7046fa89ac7102beed2fb315bd4bf4db3acedb77
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-03-31 22:26:53 +0530

    Create count odd number in an interval range.cpp

---
+++ count odd number in an interval range.cpp
@@ -0,0 +1,31 @@
+//i have written in two programs 
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+     int count=(high-low)/2;
+        if(high%2!=0||low%2!=0)
+            count++;
+            
+      return count;    
+    }
+};
+
+
+
+
+
+
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+     int count=(high-low)/2;
+        if(high%2!=0)
+            count++;
+        else if(low%2!=0)
+            count++;
+            
+      return count;    
+    }
+};
