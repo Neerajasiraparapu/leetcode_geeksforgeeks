f8c36a1c799f751ac518ebce187c396c7ceb961d
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2023-06-08 21:10:39 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (6.21%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int pos=0,neg=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                pos++;
+            }
+            else if(nums[i]<0){
+                neg++;
+            }
+            else{
+                return 0;
+            }
+        }
+        if(neg%2!=0){
+            return -1;
+        }
+        return 1;
+    }
+}
